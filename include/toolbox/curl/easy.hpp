a671f13370b15c9729ae2d5731be8fb367884801
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-09-01 19:50:45 +0200

    curl: easy: use option_def value() method
    and add basic support for type translation between c++ and c/curl

--- include/toolbox/curl/easy.hpp
+++ include/toolbox/curl/easy.hpp
@@ -11,20 +11,43 @@ namespace curl
 namespace options
 {
 
+namespace detail
+{
+
+template<typename T>
+auto to_curl_type(const T& in)
+{
+	return in;
+}
+
+template<> auto to_curl_type<std::string>(const std::string &in)
+{
+	return in.c_str();
+}
+
+} // namespacec detail
+
 template<int E, typename T>
 struct option_def
 {
 	using type_t = T;
 	static constexpr auto option = static_cast<CURLoption>(E);
-	const T &value;
 
 	option_def(const T &value) :
-		value (value) {}
+		value_ (value) {}
+
+	auto value() const
+	{
+		return detail::to_curl_type(value_);
+	}
+
+private:
+	const T &value_;
 };
 
 using verbose = option_def<CURLOPT_VERBOSE, long>;
-using url = option_def<CURLOPT_URL, const char*>;
-using postfields = option_def<CURLOPT_POSTFIELDS, const char*>;
+using url = option_def<CURLOPT_URL, std::string>;
+using postfields = option_def<CURLOPT_POSTFIELDS, std::string>;
 using writefunction = option_def<CURLOPT_WRITEFUNCTION, size_t(*)(char *, size_t, size_t, void *)>;
 using writedata = option_def<CURLOPT_WRITEDATA, void *>;
 
@@ -56,7 +79,7 @@ public:
 	template<typename T>
 	auto set(const T &value)
 	{
-		return curl_easy_setopt(curl_, T::option, value.value);
+		return curl_easy_setopt(curl_, T::option, value.value());
 	}
 
 	CURLcode perform()
